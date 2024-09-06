90773f0acfe59e86403dffd57a75924194c6bf48
ManuelVilla <manuelavt1912@gmail.com>
2024-09-06 10:34:27 -0600

    Proyecto Terminado

---
+++ src/visual.java
@@ -0,0 +1,81 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Scanner;
+
+public class visual {
+    private JTextField textFNum1;
+    private JTextField textFNum2;
+    private JButton sumarButton;
+    private JButton restaButton;
+    private JButton multiplicacionButton;
+    private JButton divisionButton;
+    private JLabel jLabRes;
+    private JPanel pnlInicio;
+    private JButton borrarButton;
+
+
+    public visual() {
+        sumarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Scanner scan = new Scanner(System.in);
+                Double num1 = Double.parseDouble(textFNum1.getText());
+                Double num2 = Double.parseDouble(textFNum2.getText());
+                double res = num1 + num2;
+                jLabRes.setText("" + res);
+            }
+        });
+        borrarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jLabRes.setText("");
+                textFNum1.setText("");
+                textFNum2.setText("");
+            }
+        });
+        restaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Scanner scan = new Scanner(System.in);
+                Double num1 = Double.parseDouble(textFNum1.getText());
+                Double num2 = Double.parseDouble(textFNum2.getText());
+                double res = num1 - num2;
+                jLabRes.setText("" + res);
+            }
+        });
+        multiplicacionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Scanner scan = new Scanner(System.in);
+                Double num1 = Double.parseDouble(textFNum1.getText());
+                Double num2 = Double.parseDouble(textFNum2.getText());
+                double res = num1 * num2;
+                jLabRes.setText("" + res);
+            }
+        });
+        divisionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Scanner scan = new Scanner(System.in);
+                Double num1 = Double.parseDouble(textFNum1.getText());
+                Double num2 = Double.parseDouble(textFNum2.getText());
+                double res = num1 / num2;
+                jLabRes.setText("" + res);
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+JFrame frame = new JFrame("CALCULADORA");
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+visual vis = new visual();
+frame.setSize(500,300); //Tamaño de la ventana
+        frame.setContentPane(new visual().pnlInicio);
+
+frame.pack();
+frame.setVisible(true);
+
+    }
+}
